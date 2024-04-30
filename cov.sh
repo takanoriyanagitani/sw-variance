4fa97a6f1a5443228176c1bc223abfad597bab6e
yanagitani <takanori.yanagitani@gmail.com>
2024-05-01 08:02:51 +0900

    test: coverage for linux

--- cov.sh
+++ cov.sh
@@ -36,13 +36,45 @@ covDarwin() {
 		-instr-profile ./.build/debug/codecov/default.profdata
 }
 
+covLinux() {
+	local name
+	name=$1
+	readonly name
+
+	local prefix
+	prefix="./.build/debug/${name}.xctest"
+	readonly prefix
+
+	swift \
+		test \
+		--quiet \
+		--parallel \
+		--enable-code-coverage \
+		--jobs ${jobs} ||
+		exec sh -c 'echo TEST FAILURE; exit 1'
+
+	llvm-cov \
+		export \
+		-format=lcov \
+		"${prefix}" \
+		-instr-profile ./.build/debug/codecov/default.profdata |
+		cat >./cov.lcov
+
+	llvm-cov \
+		report \
+		--ignore-filename-regex=.build \
+		--summary-only \
+		"${prefix}" \
+		-instr-profile ./.build/debug/codecov/default.profdata
+}
+
 numcpus=8
 jobs=$((${numcpus} - 1))
 
 targets | while read line; do
 	case $(uname -o) in
 	GNU/Linux)
-		exec sh -c 'echo TODO; exit 1'
+		covLinux "${line}"
 		;;
 	Darwin)
 		covDarwin "${line}"
